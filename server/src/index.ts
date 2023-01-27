7c1c57a1416101f413bbb3c45524bb4b068d1e06
mpark1994 <mpark1994@outlook.com>
2023-01-27 17:35:07 -0500

    decks frontend & css

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,17 +3,27 @@ config()
 
 import express, { Request, Response} from "express"
 import mongoose from 'mongoose'
-
+import cors from 'cors'
 import Deck from "./models/Deck"
 
 const PORT = 5000
 
 const app = express()
+//for bypassing CORS error
+app.use(cors({
+    origin: '*'
+}))
 // Express middleware - allows JSON post requests
 app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!")
+})
+
+app.get("/decks", async (req: Request, res: Response) => {
+    // Fetch all decks
+    const decks = await Deck.find()
+    res.json(decks)
 })
 
 app.post("/decks", async (req: Request, res: Response) => {
